c985a772ee0f6dccc8ad192f11aea3177f5eb27a
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-10 14:29:39 -0300

    Adicionada funcionalidade de execução em loop

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,47 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("--------------------------------");
-
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("S - Sair");
-
-            Console.WriteLine();
-            Console.Write("Escolha uma opção: ");
-
-            string opcao = Console.ReadLine().ToUpper();
-
-            if (opcao == "S") {
-                return;
-            }
-
-            Console.Write("Digite o primeiro número: ");
-            string primeiroNumeroString = Console.ReadLine();
-            int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
-
-            Console.Write("Digite o segundo número: ");
-            string segundoNumeroString = Console.ReadLine();
-            int segundoNumero = Convert.ToInt32(segundoNumeroString);
-
-            int resultado = 0;
-
-
-            // se / senao / senao se
-            if (opcao == "1")
+            while (true) // enquanto verdadeiro for verdadeiro
             {
-                resultado = primeiroNumero + segundoNumero;
-            }
-            else
-            {
-                resultado = primeiroNumero - segundoNumero;
-            }
+                Console.Clear();
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("--------------------------------");
 
-            Console.WriteLine(resultado);
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("S - Sair");
 
-            Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao == "S")
+                {
+                    break;
+                }
+
+                Console.Write("Digite o primeiro número: ");
+                string primeiroNumeroString = Console.ReadLine();
+                int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
+
+                Console.Write("Digite o segundo número: ");
+                string segundoNumeroString = Console.ReadLine();
+                int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+                int resultado = 0;
+
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+                else
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine("Resultado: " + resultado);
+                Console.WriteLine("--------------------------------");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                {
+                    break;
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
